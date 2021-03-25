b9b162be01dea67d48b9cad4771e78c2144d16b4
leejb <leejb@leejbui-MacBook-Pro.local>
2021-03-25 12:32:04 +0900

    second commit

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 git add .
-git commit -m "first commit"
+git commit -m "second commit"
 git branch -M main
 git remote add origin https://github.com/bell2017/test2.git
 git push -u origin main
